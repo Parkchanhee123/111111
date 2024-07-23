510750d1735d6f798c64d1918ceea951a451cd65
parkchanhee <pchh213@naver.com>
2024-07-23 11:35:52 +0900

    Your commit message

--- routes/block.js
+++ routes/block.js
@@ -11,14 +11,31 @@ var goto = require('../util/goto.js');
 var db_connect = require('../db/db_connect.js');
 var db_sql = require('../db/db_sql.js');
 
+// /block
 router
-    .get("/block1", (req, res) => {      
-        goto.go(req, res, { 'center': 'block/block1' });
+    .get("/block1",(req,res)=>{  
+        goto.go(req,res,{'center':'block/block1'});
     })
-
-    .get("/block2", (req, res) => {      
-        goto.go(req, res, { 'center': 'block/block2' });
-    })
-
+    .get("/block2",(req,res)=>{   
+        let id = req.query.id;
+        conn = db_connect.getConnection();
+        conn.query(db_sql.cust_select_one, id, (err, result, fields) => {
+            try{
+                if(err){
+                    console.log('Select Error');
+                    throw err;
+                }else{
+                    console.log(result);
+                    custinfo = result[0];
+                    console.log(custinfo);
+                    goto.go(req,res,{'center':'block/block2','custinfo':custinfo});
+                }
+            }catch(e){
+                console.log(e);
+            }finally{
+                db_connect.close(conn);
+            }
+        });
+    });
 
 module.exports = router;
